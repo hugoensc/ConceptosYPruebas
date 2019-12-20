80efe32d77837d8216fd6bd7802bab16ea82b24e
Hugo Santamaria <hugo.santamaria@alianzacorp.mx>
2019-12-20 13:43:55 -0600

    FristCommit

---
+++ ConceptosYPruebas/Base.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConceptosYPruebas
+{
+    class Base
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public Base(string name)
+        {
+            Name = name;
+        }
+
+        public void printName()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+}
---
+++ ConceptosYPruebas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConceptosYPruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Base b = new Base("Hugo Enrique");
+            Base a = b;
+
+            b.Name = "Cristina Citlalli";
+
+            b.printName();
+            a.printName();
+
+
+            Console.ReadKey();  
+        }
+    }
+}
